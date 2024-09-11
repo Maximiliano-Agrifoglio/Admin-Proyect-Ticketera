663f60d3435b7af584810fd8213ae42e28b11534
Maximiliano-Agrifoglio <maxiagrifoglio966@gmail.com>
2024-09-11 17:30:15 -0300

    creando el servidor

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import server from './server'
+import colors from 'colors'
+const port = process.env.PORT || 5000 
+
+server.listen(port, () => {
+    console.log(colors.cyan(`servidor corriendo en el puerto ${port}`))
+});
