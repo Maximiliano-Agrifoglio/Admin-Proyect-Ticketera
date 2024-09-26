bb0683dadbfbbb2d9ee4d15c2e74e085014015b7
Maximiliano-Agrifoglio <maxiagrifoglio966@gmail.com>
2024-09-26 17:13:39 -0300

    continue crud RS/UPDATE

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -23,6 +23,35 @@ import colors from 'colors'
                 console.log(colors.red(error));
             }
         }
+
+        static getProjectByid =  async (req : Request, res : Response) => {
+            const { id } = req.params;
+            try {
+                const project = await Project.findById(id);
+                if (!project) {
+                    const error = new Error('Proyecto no encontrado');
+                    return res.status(404).json({error: error.message});
+                }
+                res.json(project);
+            } catch (error) {
+                console.log(colors.red(error));
+            }
+        }
+
+        static updateProject =  async (req : Request, res : Response) => {
+            const { id } = req.params;
+            try {
+                const project = await Project.findByIdAndUpdate(id, req.body);
+                if (!project) {
+                    const error = new Error('Proyecto no encontrado');
+                    return res.status(404).json({error: error.message});
+                }
+                await project.save();
+                res.json(project);
+            } catch (error) {
+                console.log(colors.red(error));
+            }
+        }
     };
 
 export default ProjectController;
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,21 +1,37 @@
 import { Router } from "express";
 import ProjectController from "../controllers/projectController";
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 import handleImputsErrors from "../middleware/validation";
 
 const router = Router();
 
 router.post('/',
-     body('projectName')
+          body('projectName')
          .notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
           body('clientName')
          .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
-         body('description')
+          body('description')
          .notEmpty().withMessage('La Descripción del Proyecto es Obligatoria'),
 
      handleImputsErrors,
      ProjectController.createProject);
      
 router.get('/', ProjectController.GetAllProjects );
+
+router.get('/:id',
+       param('id').isMongoId().withMessage('ID no valido'),
+       handleImputsErrors,
+       ProjectController.getProjectByid);
+
+router.put('/:id',
+         param('id').isMongoId().withMessage('ID no valido'),
+         body('projectName')
+        .notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+         body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+         body('description')
+        .notEmpty().withMessage('La Descripción del Proyecto es Obligatoria'),
+ handleImputsErrors,
+ ProjectController.updateProject);
 
 export default router;
