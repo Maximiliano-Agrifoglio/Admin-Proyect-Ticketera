c338e76360ff555be581c3aceef2f45501879581
Maximiliano-Agrifoglio <maxiagrifoglio966@gmail.com>
2025-01-13 17:28:24 -0300

    validando usuario

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { AuthController } from "../controllers/AuthController";
+import handleImputsErrors from "../middleware/validation";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.get('/',(req, res) => {
-    res.send('prueba de response');
-});
+router.post('/create-account',
+        body('name')
+             .notEmpty().withMessage('El nombre no puede ir vacio'),
+        body('password')
+             .isLength({min:8}).withMessage('El password debe contener 8 caracteres minimo'),
+        body('password_confirmation').custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Los passwords no son iguales')
+            }
+            return true
+        }),   
+        body('email')
+             .isEmail().withMessage('E-mail no válido'),
+        handleImputsErrors,
+        AuthController.createAccount
+);
 
 export default router;
