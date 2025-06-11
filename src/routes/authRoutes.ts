2af4b304e110cbc21703e68b58fd17cdec709718
Maximiliano-Agrifoglio <maxiagrifoglio966@gmail.com>
2025-06-11 10:19:05 -0300

    proyect standby!

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
 import handleImputsErrors from "../middleware/validation";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 
 const router = Router();
 
@@ -50,4 +50,28 @@ router.post('/forgot-password',
           AuthController.forgotPassword
 );
 
+router.post('/validate-token',
+     body('token')
+          .notEmpty().withMessage('El token no puede ir vacio'),
+          handleImputsErrors,
+          AuthController.validateToken
+);
+
+router.post('/update-password/:token',
+     param('token')
+          .isNumeric().withMessage('Token no válido'),
+           body('password')
+             .isLength({min:8}).withMessage('El password debe contener 8 caracteres minimo'),
+        body('password_confirmation').custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Los passwords no son iguales')
+            }
+            return true
+          }), 
+          handleImputsErrors,
+          AuthController.updatePasswordWithToken
+);
+
 export default router;
+
+
